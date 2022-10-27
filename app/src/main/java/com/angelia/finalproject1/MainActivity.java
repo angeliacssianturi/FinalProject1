457f04312a10e1b46c1934095a4a20fe7bc2f5d7
finalproject1 <chrismesisheila@gmail.com>
2022-10-27 21:53:05 +0700

    FinalProject1

---
+++ app/src/main/java/com/angelia/finalproject1/MainActivity.java
@@ -0,0 +1,100 @@
+package com.angelia.finalproject1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper dbHelper;
+    ArrayAdapter<String> aAdapter;
+    ListView lv1;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);dbHelper = new DatabaseHelper(this);
+        lv1 = findViewById(R.id.lv1);
+        loadTaskList();
+    }
+
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if (aAdapter==null){
+            aAdapter = new ArrayAdapter<String>(this,R.layout.row,R.id.todoTask,taskList);
+            lv1.setAdapter(aAdapter);
+        } else {
+            aAdapter.clear();
+            aAdapter.addAll(taskList);
+            aAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+        icon.setColorFilter(getResources().getColor(android.R.color.black), PorterDuff.Mode.SRC_IN);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_task: final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Tambah Task")
+                        .setMessage("Apa yang ingin kamu lakukan hari ini ?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Tambah", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertNewTask(task);
+                                loadTaskList();
+                            }
+                        })
+                        .setNegativeButton("Batal",null)
+                        .create();
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void deleteTask(View view){
+        View parent = (View) view.getParent();
+        final TextView taskTextView = (TextView) parent.findViewById(R.id.todoTask);
+        Log.e("String",(String) taskTextView.getText());
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage("Yakin ingin Menghapus?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String task = String.valueOf(taskTextView.getText());
+                        dbHelper.deleteTask(task);
+                        loadTaskList();
+                    }
+                })
+                .setNegativeButton("No",null)
+                .show();
+    }
+}
